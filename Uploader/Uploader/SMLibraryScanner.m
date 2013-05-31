912dc6bf5b7468496eb2ed0ad04236dcffe4ebe7
Zakk Hoyt <zakk@threefoldphotos.com>
2013-05-31 16:34:57 -0700

    first commit

---
+++ Uploader/Uploader/SMLibraryScanner.m
@@ -0,0 +1,13 @@
+//
+//  SMLibraryScanner.m
+//  Uploader
+//
+//  Created by Zakk Hoyt on 5/31/13.
+//  Copyright (c) 2013 Zakk Hoyt. All rights reserved.
+//
+
+#import "SMLibraryScanner.h"
+
+@implementation SMLibraryScanner
+
+@end
